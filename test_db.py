dd9c18ae2a0abdb75a92856592ad3d93301040e0
TheSimonCraft <simon.unger2007@gmail.com>
2024-09-28 14:05:33 +0200

    WIP

--- test_db.py
+++ test_db.py
@@ -2,17 +2,16 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorDistanceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait, StopWatch, run_task
 import math
 
 hub = PrimeHub()
 
-ml, mr = Motor(Port.A), Motor(Port.E, positive_direction=Direction.COUNTERCLOCKWISE)
-
-diameter = 5.6
-axle = 11.3
-
 async def curve(radius, angle, speed=100):
+        
+    diameter = 5.6
+    axle = 11.3
+
     cl, cr = radius / (radius + axle / 2), (radius + axle) / (radius + axle / 2)
     sl, sr = speed * cl, speed * cr
     d = 2 * radius * math.pi * angle / 360
@@ -21,4 +20,5 @@ async def curve(radius, angle, speed=100):
     await mr.run_angle(sr, dr)
 
 if __name__ == "__main__":
-    await curve(20, 90)
+    ml, mr = Motor(Port.A), Motor(Port.E, positive_direction=Direction.COUNTERCLOCKWISE)
+    run_task(curve(20, 90))
