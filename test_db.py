51503bbe24164ba2f75089a264ba272ff7a29ec0
TheSimonCraft <simon.unger2007@gmail.com>
2024-11-02 10:49:55 +0100

    Added requirements in test_db.py

--- test_db.py
+++ test_db.py
@@ -7,7 +7,7 @@ import umath as math
 
 hub = PrimeHub()
 
-async def curve(radius, angle, speed=200, clockwise: bool = True):
+async def arc(radius, angle, speed=200, clockwise: bool = True):
     if angle < 0: angle = -angle; speed=-speed
     diameter = 5.7
     axle = 11.3
@@ -19,6 +19,32 @@ async def curve(radius, angle, speed=200, clockwise: bool = True):
     dl, dr = cl*d/(diameter * math.pi)*360, cr*d/(diameter * math.pi)*360
     ml.run_angle(sl, (dl))
     await mr.run_angle(sr, (dr))
+
+async def yaw():
+    pass # + relative yaw
+async def straight():
+    pass # + gyro
+
+async def move_motor():
+    pass # Degrees and Rotations
+
+async def start_db():
+    pass # + gyro
+
+async def stop_db():
+    pass
+
+async def elliptical_arc():
+    pass
+
+async def steer():
+    pass # Spike Steering
+
+async def color_recognition():
+    pass
+
+async def settings():
+    # default_speed, acceleration, deceleration, PID, use_gyro
 
 async def main():
     await curve(15, 180)
